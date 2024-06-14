4efc9b477bc871f31697e8c5ed02a4eaeb73d54c
James N <randomspaceship@users.noreply.github.com>
2024-06-14 17:53:49 +1000

    hsv views

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import cv2
+import cv2 as cv
 
 window_title = "Frame"
 
-showingHsv = False
+currentlyShowing = 0
 
 mouseX = 0
 mouseY = 0
@@ -10,80 +10,118 @@ mouseY = 0
 
 def mouse_event(event, x, y, flags, param):
     global mouseX, mouseY
-    if event == cv2.EVENT_MOUSEMOVE:
+    if event == cv.EVENT_MOUSEMOVE:
         mouseX, mouseY = x, y
 
 
-cv2.namedWindow(window_title)
-cv2.setMouseCallback(window_title, mouse_event)
+cv.namedWindow(window_title)
+cv.setMouseCallback(window_title, mouse_event)
 
 
 def rtext(img, text, org):
-    cv2.putText(
+    cv.putText(
         img,
         text,
         org,
-        cv2.FONT_HERSHEY_SIMPLEX,
+        cv.FONT_HERSHEY_SIMPLEX,
         1,
         (255, 255, 255),
         4,
-        cv2.LINE_AA,
+        cv.LINE_AA,
     )
-    cv2.putText(
+    cv.putText(
         img,
         text,
         org,
-        cv2.FONT_HERSHEY_SIMPLEX,
+        cv.FONT_HERSHEY_SIMPLEX,
         1,
         (0, 0, 0),
         2,
-        cv2.LINE_AA,
+        cv.LINE_AA,
     )
 
 
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
 while True:
-    k = cv2.waitKey(1)
+    k = cv.waitKey(1)
 
-    if k == ord("h"):
-        showingHsv = True
     if k == ord("g"):
-        showingHsv = False
+        currentlyShowing = 0
+    if k == ord("h"):
+        currentlyShowing = 1
+    if k == ord("s"):
+        currentlyShowing = 2
+    if k == ord("v"):
+        currentlyShowing = 3
+    if k == ord("H"):
+        currentlyShowing = 4
 
-    imgRaw = cv2.imread("test.png")
+    # imgRaw = cv.imread("test.png")
+    ret, rawImg = cap.read()
+    if ret == False:
+        break
 
-    img = imgRaw if showingHsv else imgRaw
+    hsvImg = cv.cvtColor(rawImg, cv.COLOR_BGR2HSV_FULL)
+    hsvHueOnly = hsvImg.copy()
+    hsvHueOnly[:, :, 1] = 255
+    hsvHueOnly[:, :, 2] = 255
+    hsvHueOnly = cv.cvtColor(hsvHueOnly, cv.COLOR_HSV2BGR_FULL)
+    hsvSatOnly = cv.cvtColor(hsvImg[:, :, 1], cv.COLOR_GRAY2BGR)
+    hsvValOnly = cv.cvtColor(hsvImg[:, :, 2], cv.COLOR_GRAY2BGR)
 
-    rtext(img, "HSV" if showingHsv else "RGB", (5, 30))
+    # pixel is in BGR!
+    rgbPixel = rawImg[mouseY, mouseX]
+    hsvPixel = hsvImg[mouseY, mouseX]
+
+    showFrame = rawImg
+    txt = "RGB"
+    if currentlyShowing == 1:
+        showFrame = hsvHueOnly
+        txt = "Hue"
+    elif currentlyShowing == 2:
+        showFrame = hsvSatOnly
+        txt = "Sat"
+    elif currentlyShowing == 3:
+        showFrame = hsvValOnly
+        txt = "Val"
+    elif currentlyShowing == 4:
+        showFrame = hsvImg
+        txt = "HSV"
+    showFrame = cv.flip(showFrame, 1)
+    rtext(showFrame, txt, (5, 30))
     rtext(
-        img,
+        showFrame,
         f"X:{mouseX:04d}, Y:{mouseY:04d}",
         (5, 60),
     )
-    # pixel is in BGR!
-    pixel = img[mouseY, mouseX]
     rtext(
-        img,
-        f"{pixel[2]:03d},{pixel[1]:03d},{pixel[0]:03d}",
+        showFrame,
+        f"R{rgbPixel[2]:03d} G{rgbPixel[1]:03d} B{rgbPixel[0]:03d}",
         (5, 90),
     )
-
-    cv2.imshow(window_title, img)
+    rtext(
+        showFrame,
+        f"H{hsvPixel[0]:03d} S{hsvPixel[1]:03d} V{hsvPixel[2]:03d}",
+        (5, 120),
+    )
+    cv.imshow(window_title, showFrame)
 
     if k == ord(" ") or k == ord("q"):
         break
 
+cap.release()
+cv.destroyAllWindows()
 
 # import numpy as np
-# import cv2 as cv
+# import cv as cv
 
-# # cap = cv.VideoCapture(0)
-# # if not cap.isOpened():
-# #     print("Cannot open camera")
-# # exit()
 
 # while True:
 #     # Capture frame-by-frame
-#     # ret, frame = cap.read()
 #     ret = True
 #     frame = cv.imread("test.png")
 
@@ -99,5 +137,3 @@ while True:
 #         break
 
 # # When everything done, release the capture
-# # cap.release()
-# cv.destroyAllWindows()
